32e83f4e22198b902ff0202cea79fa3f1cef4884
AlexisTubulekas <alexistubulekas@gmail.com>
2016-05-18 10:33:56 +0200

    added distance function and getCurrentLocation function

--- map.js
+++ map.js
@@ -38,3 +38,39 @@ function createMarkers(){
   });
 }
 
+var getLocation = function(map){
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+      function(position){
+        var currentPos = {
+          lat: position.coords.latitude,
+          lng : position.coords.longitude
+
+        };
+        map.setCenter(currentPos);
+        console.log(lat)
+        return currentPos;
+      }/*,
+      function(error){
+        console.log('Error: ',error);
+      }*/)
+  }
+
+var rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+var getDistance = function(p1Lat, p1Lng, p2Lat, p2Lng) {
+  var R = 6378137; // Earth’s mean radius in meter
+  var dLat = rad(p2Lat - p1Lat);
+  var dLong = rad(p2Lng - p1Lng);
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1Lat) * Math.cos(rad(p2Lat))) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  var distance = getDistance(sq1Lat, sq1Lng, sq2Lat, sq2Lng).toFixed(2)
+  return distance// returns the distance in meter
+};
+
+
