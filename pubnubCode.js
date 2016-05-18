79399ac450499d2c9a9754ba96191d87cc367d76
Riverbell <emba93@live.se>
2016-05-18 15:51:49 +0200

    Minor fix of uuid

--- pubnubCode.js
+++ pubnubCode.js
@@ -7,7 +7,7 @@ function game(usrAlias) {
 	        d = Math.floor(d/16);
 	        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
 	    });
-	    return usrAlias+uuid;
+	    return usrAlias+'-'+uuid;
 	    //return 'Emma';
 	};
 
