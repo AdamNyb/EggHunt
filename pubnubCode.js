ab9a31b346f3e69f07b60be53e01398b6614d0d2
Riverbell <emba93@live.se>
2016-05-18 16:20:39 +0200

    Fixed so that the uuid is not set on initialization, but afterwards and cleaned up some code

--- pubnubCode.js
+++ pubnubCode.js
@@ -2,91 +2,101 @@ var eggChannel = 'eggChannel';
 var scoreChannel = 'scoreChannel';
 var positionChannel = 'positionChannel';
 var gameChannel_Group = 'gameChannel_Group';
+var user = {};
 
-function game(usrAlias) {
 
-	function generateUUID(usrAlias) {
-	    var d = new Date().getTime();
-	    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	        var r = (d + Math.random()*16)%16 | 0;
-	        d = Math.floor(d/16);
-	        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	    });
-	    return usrAlias+'-'+uuid;
-	    //return 'Emma';
-	};
+var pubnub_data = PUBNUB.init({ // initializes pubnub
+	publish_key: 'pub-c-bf94e267-7c61-4c6d-b237-727f398f655d',
+	subscribe_key: 'sub-c-237e52d0-1b4c-11e6-be83-0619f8945a4f',
+	//uuid: user.uuid,
+	//state: {
+	//	"alias" : user.alias
+	//}
+});
 
-	var user = {};
+function generateUUID(usrAlias) {
+	//generates a random uuid
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return usrAlias+'-'+uuid;
+};
+
+
+
+function setNewUUID(usrAlias) {
+	// sets a new uuid, ouside of the initialization
+	if (usrAlias == "") {
+		usrAlias = 'Eggbert';
+	}
 	user.uuid = generateUUID(usrAlias);
 	user.alias = usrAlias;
 
-	var pubnub_data = PUBNUB.init({ // initializes pubnub
-		publish_key: 'pub-c-bf94e267-7c61-4c6d-b237-727f398f655d',
-		subscribe_key: 'sub-c-237e52d0-1b4c-11e6-be83-0619f8945a4f',
-		uuid: user.uuid,
-		state: {
-			"alias" : user.alias
-		}
-	});
-
-	
-
-	// creates channel group
-	pubnub_data.channel_group_add_channel({
-	    channel: eggChannel,
-	    channel_group: gameChannel_Group
-	});
-	pubnub_data.channel_group_add_channel({
-	    channel: scoreChannel,
-	    channel_group: gameChannel_Group
-	});
-	pubnub_data.channel_group_add_channel({
-	    channel: positionChannel,
-	    channel_group: gameChannel_Group
-	});
-
-	var uuids = [];
-	var scoreboard = {};
-
-
-	// subscribes to channel group
-	pubnub_data.subscribe({
-	    channel_group: gameChannel_Group,
-	    callback: function(m){
-	        //console.log("subscribe callback",m);
-	    }
-	});
-	// Get List of Occupants and Occupancy Count.
-	pubnub_data.here_now({
-	    channel_group : gameChannel_Group,
-	    callback : function(m){
-	    	console.log("HELLO");
-	        console.log(m);
-	    }
-	});	
-
-	// pubnub_data.subscribe({
-	// 	channel: eggChannel,
-	// 	//noheresync: true,
-	// 	message: function(m){
-	// 		console.log("Message");
-	// 		console.log(m);
-	// 	},
-	// 	presence: function(m) {
-	// 		//console.log("presence");
-	// 		if (m.action == 'join') {
-	// 			console.log("someone joined!");
-	// 			console.log(m);
-	// 			scoreboard[m.uuid] = 0;
-	// 			publish(scoreboard,scoreChannel);
-	// 		} else if (m.action == 'leave') {
-	// 			console.log("someone left!");
-	// 		}
-	// 	}
-	// });
-
-
+	// get/create/store UUID
+	PUBNUB.db.get('session') || (function(){ 
+	    //var uuid = generateUUID('MUPP'); 
+	    PUBNUB.db.set('session', user.uuid); 
+	    //return uuid; 
+	})();
+	console.log("User uuid",user.uuid);
 }
+
+// creates channel group
+pubnub_data.channel_group_add_channel({
+    channel: eggChannel,
+    channel_group: gameChannel_Group
+});
+pubnub_data.channel_group_add_channel({
+    channel: scoreChannel,
+    channel_group: gameChannel_Group
+});
+pubnub_data.channel_group_add_channel({
+    channel: positionChannel,
+    channel_group: gameChannel_Group
+});
+
+
+// subscribes to channel group
+pubnub_data.subscribe({
+    channel_group: gameChannel_Group,
+    callback: function(m){
+        //console.log("subscribe callback",m);
+    }
+});
+// Get List of Occupants and Occupancy Count.
+pubnub_data.here_now({
+    channel_group : gameChannel_Group,
+    callback : function(m){
+    	console.log("HELLO");
+        console.log(m);
+    }
+});	
+
+// pubnub_data.subscribe({
+// 	channel: eggChannel,
+// 	//noheresync: true,
+// 	message: function(m){
+// 		console.log("Message");
+// 		console.log(m);
+// 	},
+// 	presence: function(m) {
+// 		//console.log("presence");
+// 		if (m.action == 'join') {
+// 			console.log("someone joined!");
+// 			console.log(m);
+// 			scoreboard[m.uuid] = 0;
+// 			publish(scoreboard,scoreChannel);
+// 		} else if (m.action == 'leave') {
+// 			console.log("someone left!");
+// 		}
+// 	}
+// });
+
+
+
 
 function publish(text,channel) {
 	if (!text) return;
@@ -102,30 +112,6 @@ function publish(text,channel) {
 	  }
 	});
 }
-
-function getHistory(channel, count) {
-	// gets history for one specified channel
-	// input: what channel and number of messages
-	// since we are only able to get hold of the messages through 
-	// the callback, maybe it's better to have a history function 
-	// for each channel that we need to retrieve messages from?
-	// OR maybe it's not so convienient to have a general history function at all?
-	// maybe it's better to use the .history command when it's needed?
-	console.log("Gets history");
-	pubnub_data.history({
-	  channel: channel,
-	  count: count,
-	  callback: function(messages) {
-	    pubnub_data.each(messages[0], function(m) {
-	    	console.log(m);
-	    	//console.log(m.text.lng);
-	    });
-	    eggs = messages;
-	    console.log("eggs2",eggs);
-	  }
-	});
-};
-
 
 
 var Sthlm = {lat: 59.332595, lng: 18.065193};
